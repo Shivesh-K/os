5ea2c19fdaebea03031af9f3bb6577a61bc0a6d9
Shivesh-Kaundinya <54278503+Shivesh-Kaundinya@users.noreply.github.com>
2021-02-03 21:08:37 +0530

    Update Task1.c

--- LS3/Task1.c
+++ LS3/Task1.c
@@ -5,6 +5,38 @@
 #include <assert.h>
 #include <stdbool.h>
 
+struct Node {
+    double val;
+    struct Node* next;
+};
+
+struct Node *userTimes, 
+    *sysTimes, 
+    *idleTimes, 
+    *freeMems,
+    *freeMemPers, 
+    *readRates, 
+    *writeRates, 
+    *cntxSwitchRates, 
+    *procCreateRates;
+    
+struct Node* insert(struct Node *head, double val) {
+    struct Node *n = (struct Node*) malloc(sizeof(struct Node));
+    n->val = val;
+    n->next = head;
+    return n;
+}
+
+double calcAvg(struct Node* head, int l) {
+    double total = 0;
+    int c = l;
+    while(head && l--) {
+        total += head->val;
+        head = head->next;
+    }
+    return total / c;
+}
+
 void v1()
 {
     // Open all the required files
@@ -56,14 +88,17 @@ void processorTime()
 {
     FILE *stat = fopen("/proc/stat", "r");
     long long total = 0, ut, st, it, t;
-    float up, sp, ip;
-    for (int i = 1; i <= 8; ++i)
+    double up, sp, ip;
+    char line[100], ts[10];
+    fscanf(stat, "%s", ts);
+
+    for (int i = 1; i <= 10; ++i)
     {
         fscanf(stat, "%lld", &t);
         if (i == 1)
             ut = t;
         else if (i == 3)
-            st == t;
+            st = t;
         else if (i == 4)
             it = t;
         total += t;
@@ -71,19 +106,25 @@ void processorTime()
     up = 100.0 * ut / total;
     sp = 100.0 * st / total;
     ip = 100.0 * it / total;
-    printf("User time percentage: %f\nSystem time percentage: %f\nIdle time percentage: %f\n", up, sp, ip);
+    //printf("User time percentage: %f\nSystem time percentage: %f\nIdle time percentage: %f\n", up, sp, ip);
+    userTimes = insert(userTimes, up);
+    sysTimes = insert(sysTimes, sp);
+    idleTimes = insert(idleTimes, ip);
     fclose(stat);
 }
 
 void freeMemory()
 {
-    FILE *meminfo = fopen("/proc/stat", "r");
-    long long freeMem, total;
-    float freePer;
-    fscanf(meminfo, "%lld", &total);
-    fscanf(meminfo, "%lld", &freeMem);
+    FILE *meminfo = fopen("/proc/meminfo", "r");
+    long long freeMem, total, t;
+    double freePer;
+    char ts[100];
+    fscanf(meminfo, "%s %lld kB", ts, &total);
+    fscanf(meminfo, "%s %lld kB", ts, &freeMem);
     freePer = 100.0 * freeMem / total;
-    printf("Free Memory: %lld\nFree memory perentage: %f\n", freeMem, freePer);
+    //printf("Total memory: %lld\nFree memory: %lld\nFree memory perentage: %f\n", total, freeMem, freePer);
+    freeMems = insert(freeMems, 1.0 * freeMem);
+    freeMemPers = insert(freeMemPers, freePer);
     fclose(meminfo);
 }
 
@@ -91,20 +132,19 @@ void diskRW()
 {
     FILE *diskstats = fopen("/proc/diskstats", "r");
     long long readS, readT, writeS, writeT, t;
-    float readR, writeR;
+    double readR, writeR;
     char line[100], ts[100];
     size_t n = 0;
     while (!feof(diskstats))
     {
-        fscanf(diskstats, "%[^\n]", line);
-        sscanf(line, "%lld", &t);
-        sscanf(line, "%lld", &t);
-        sscanf(line, "%s", ts);
+        fscanf(diskstats, "%lld", &t);
+        fscanf(diskstats, "%lld", &t);
+        fscanf(diskstats, "%s", ts);
         if (strcmp(ts, "sda") == 0)
         {
-            for (int i = 1; i <= 8; ++i)
+            for (int i = 1; i <= 11; ++i)
             {
-                sscanf(line, "%lld", &t);
+                fscanf(diskstats, "%lld", &t);
                 if (i == 3)
                     readS = t;
                 else if (i == 4)
@@ -116,10 +156,15 @@ void diskRW()
             }
             break;
         }
+        else {
+            fscanf(diskstats, "%[^\n]s", ts);
+        }
     }
-    readR = 1000.0 * readS / readT;
-    writeR = 1000.0 * writeS / writeT;
-    printf("Read rate: %f\nWrite rate: %f\n", readR, writeR);
+    readR = readS / readT;
+    writeR = writeS / writeT;
+    //printf("Read rate: %f reads per second\nWrite rate: %f writes per second\n", readR, writeR);
+    readRates = insert(readRates, readR);
+    writeRates = insert(writeRates, writeR);
     fclose(diskstats);
 }
 
@@ -128,23 +173,25 @@ void contextSwitches()
     FILE *stat = fopen("/proc/stat", "r");
     long long switches, time;
     double rate;
-    char line[100], t[10];
-    size_t n = 0;
+    char line[1000], t[10];
     while (!feof(stat))
     {
-        fscanf(stat, "%[^\n]", line);
-        sscanf(line, "%s", t);
+        fscanf(stat, "%s", t);
         if (strcmp(t, "ctxt") == 0)
         {
-            sscanf(line, "%lld", &switches);
+            fscanf(stat, "%lld", &switches);
         }
         else if (strcmp(t, "btime") == 0)
         {
-            sscanf(line, "%lld", &time);
+            fscanf(stat, "%lld", &time);
+        }
+        else {
+            fscanf(stat, "%[^\n]s", line);
         }
     }
     rate = 1.0 * switches / time;
-    printf("Context switching rate: %lf\n", rate);
+    //printf("Context switching rate: %lf switches per second\n", rate);
+    cntxSwitchRates = insert(cntxSwitchRates, rate);
     fclose(stat);
 }
 
@@ -153,29 +200,31 @@ void processes()
     FILE *stat = fopen("/proc/stat", "r");
     long long process, time;
     double rate;
-    char line[100], t[10];
-    size_t n = 0;
+    char line[1000], t[20];
     while (!feof(stat))
     {
-        fscanf(stat, "%[^\n]", line);
-        sscanf(line, "%s", t);
+        fscanf(stat, "%s", t);
         if (strcmp(t, "processes") == 0)
         {
-            sscanf(line, "%lld", &process);
+            fscanf(stat, "%lld", &process);
         }
         else if (strcmp(t, "btime") == 0)
         {
-            sscanf(line, "%lld", &time);
+            fscanf(stat, "%lld", &time);
+        }
+        else {
+            fscanf(stat, "%[^\n]s", line);
         }
     }
     rate = 1.0 * process / time;
-    printf("Process creation rate: %lf\n", rate);
+    //printf("Process creation rate: %lf processes per second\n", rate);
+    procCreateRates = insert(procCreateRates, rate);
     fclose(stat);
 }
 
 void delay(int s)
 {
-    int ms = 1000 * s;
+    int ms = 1000000 * s;
     clock_t start = clock();
     while (clock() < start + ms)
         ;
@@ -183,6 +232,7 @@ void delay(int s)
 
 void v2(int readRate, int writeRate)
 {
+    int r = writeRate / readRate;
     for (int i = 0; true; ++i)
     {
         processorTime();
@@ -190,20 +240,44 @@ void v2(int readRate, int writeRate)
         diskRW();
         contextSwitches();
         processes();
-        delay(2);
+        
+        if(i && i % r == 0) {
+            printf("---------------------------------------------\n");
+            printf("ROUND %d\n", i/3);
+            printf("User time: %lf percent \n", calcAvg(userTimes, r)); 
+            printf("System time: %lf percent \n", calcAvg(sysTimes, r)); 
+            printf("Idle time: %lf percent \n", calcAvg(idleTimes, r)); 
+            printf("Free memory: %lf kB \n", calcAvg(freeMems, r));
+            printf("Free memory percentage: %lf percent \n", calcAvg(freeMemPers, r)); 
+            printf("Read rate: %lf reads/sec \n", calcAvg(readRates, r));
+            printf("Write rate: %lf writes/sec \n", calcAvg(writeRates, r));
+            printf("Context switching rate: %lf switches/sec \n", calcAvg(cntxSwitchRates, r)); 
+            printf("Process creation rate: %lf processes/sec \n", calcAvg(procCreateRates, r));
+            printf("---------------------------------------------\n");
+        }
+        delay(readRate);
     }
 }
 
 int main(int argc, char *argv[])
 {
-    --argc, ++argv;
+    userTimes = NULL; 
+    sysTimes = NULL;
+    idleTimes = NULL; 
+    freeMems = NULL;
+    freeMemPers = NULL; 
+    readRates = NULL;
+    writeRates = NULL;
+    cntxSwitchRates = NULL; 
+    procCreateRates = NULL;
+    --argc;
     if (argc == 0)
         v1();
     else
     {
         int readRate, writeRate;
-        sscanf(*argv, "%d", &readRate);
-        sscanf(*(argv + 1), "%d", &writeRate);
+        sscanf(argv[1], "%d", &readRate);
+        sscanf(argv[2], "%d", &writeRate);
         v2(readRate, writeRate);
     }
 }
