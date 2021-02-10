c8f888efccc6e99994f4dea2d5d4e68e6d6226db
Shivesh-Kaundinya <54278503+Shivesh-Kaundinya@users.noreply.github.com>
2021-02-10 10:11:28 +0530

    Update simulate101.sh

--- LS4/simulate101.sh
+++ LS4/simulate101.sh
@@ -3,62 +3,61 @@ declare -a PG_FRM_SIZES=("16" "32" "64" "128" "256")
 declare -a PG_FRM_COUNT=("16" "32" "64" "128" "256" "512")
 declare -a POLICY=("FIFO" "NRU" "LRU" "Opt")
 
-V1="f"
-V2="f"
-V3="f"
-V4="f"
-
 # Check if the provided problem size is correct
+B="f"
 for i in "${PRBLM_SIZES[@]}"
 do 
     if [ "$1" = "$i" ] 
     then
-        V1="t"
+        B="t"
     fi
 done
-if [ "$V1" = "f" ]
+if [ "$B" = "f" ]
 then
     echo "ERROR: Incorrect problem size provided" 
     exit 1
 fi
 
 # Check if the provided page frame size is correct
+B="f"
 for i in "${PG_FRM_SIZES[@]}"
 do 
     if [ "$2" = "$i" ] 
     then
-        V2="t"
+        B="t"
     fi
 done
-if [ "$V2" = "f" ]
+if [ "$B" = "f" ]
 then
     echo "ERROR: Incorrect page frame size provided" 
     exit 1
 fi
 
 # Check if the provided page frame count is correct
+B="f"
 for i in "${PG_FRM_COUNT[@]}"
 do 
     if [ "$3" = "$i" ] 
     then
-        V3="t"
+        B="t"
     fi
 done
-if [ "$V3" = "f" ]
+if [ "$B" = "f" ]
 then
     echo "ERROR: Incorrect page frame count provided" 
     exit 1
 fi
 
 # Check if the provided policy is correct
+B="f"
 for i in "${POLICY[@]}"
 do 
     if [ "$4" = "$i" ] 
     then
-        V4="t"
+        B="t"
     fi
 done
-if [ "$V4" = "f" ]
+if [ "$B" = "f" ]
 then
     echo "ERROR: Incorrect policy provided" 
     exit 1
